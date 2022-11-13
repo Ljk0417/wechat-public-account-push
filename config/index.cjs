813dc618ad56f33503e87e1026f30fa710001eee
Ljk0417 <108845175+Ljk0417@users.noreply.github.com>
2022-11-13 13:46:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'owSRF6CBlvsZ20aodgnUz15JNhwY',
+      id: 'owSRF6B6F8iiQzgYwOaYKJdvpUhI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'iRTaqF-7F-oPw5ucY_yUC_gRQLJthi_IY8Ypt_wkpII',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
